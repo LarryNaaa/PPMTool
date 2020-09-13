fbda1e5a993cbd288e87bf163a91df2c723f2174
LarryNaaa <najinyu08@outlook.com>
2020-09-13 13:40:10 -0400

    first commit

---
+++ PPMTool/src/main/java/com/jinyu/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.jinyu.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
