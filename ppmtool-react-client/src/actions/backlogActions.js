dffca68dde77d766933effaa30c971aa9d8652e7
LarryNaaa <najinyu08@outlook.com>
2020-09-18 19:32:26 -0400

    front end delete pt

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async (
   dispatch
@@ -65,6 +70,20 @@ export const updateProjectTask = (
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data,
+    });
+  }
+};
+
+export const deleteProjectTask = (backlog_id, pt_id) => async (dispatch) => {
+  if (
+    window.confirm(
+      `You are deleting project task ${pt_id}, this action cannot be undone`
+    )
+  ) {
+    await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: pt_id,
     });
   }
 };
