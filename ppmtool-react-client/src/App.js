0ea38f65238b8a6567540c9936626e2af58ea137
LarryNaaa <najinyu08@outlook.com>
2020-09-20 20:57:20 -0400

    react

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -11,6 +11,29 @@ import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken,
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    //handle logout
+    //window.location.href = "/";
+  }
+}
 
 // React-Redux 提供Provider组件，可以让容器组件拿到state。
 // 使用React-Router的项目，与其他项目没有不同之处，
@@ -24,6 +47,17 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+            {
+              //Public Routes
+            }
+
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+            {
+              //Private Routes
+            }
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
 import backlogReducer from "./backlogReducer";
+import securityReducer from "./securityReducer";
 
 // Redux 提供了一个combineReducers方法，用于 Reducer 的拆分。
 // 只要定义各个子 Reducer 函数，然后用这个方法，将它们合成一个大的 Reducer。
@@ -10,4 +11,5 @@ export default combineReducers({
   errors: errorReducer,
   project: projectReducer,
   backlog: backlogReducer,
+  security: securityReducer,
 });
